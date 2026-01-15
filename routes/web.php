7cf440c30ea6cde36d602ebedac115f91ab31122
valentasaputra <valentaofficial@gmail.com>
2026-01-15 20:22:47 +0700

    add manajemen event

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\EventController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -9,6 +11,7 @@ Route::get('/', function () {
 });
 
 Route::middleware('auth')->group(function () {
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
@@ -17,7 +20,14 @@ Route::middleware('auth')->group(function () {
         ->prefix('admin')
         ->name('admin.')
         ->group(function () {
+
             Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+            // Category Management
+            Route::resource('categories', CategoryController::class);
+
+            // Event Management
+            Route::resource('events', EventController::class);
         });
 });
 
